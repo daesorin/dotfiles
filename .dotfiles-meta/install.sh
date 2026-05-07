70f010ff33bceab7d594e43d09004c6b427a5a0c
Osaigbovo Omere <daesorin+codeberg@proton.me>
2026-05-07 11:16:18 +0100

    more updates.

--- .dotfiles-meta/install.sh
+++ .dotfiles-meta/install.sh
@@ -3,7 +3,7 @@
 
 set -e
 
-DOTFILES_REPO="https://github.com/daesorin/dotfiles.git"
+DOTFILES_REPO="https://codeberg/daesorin/dotfiles.git"
 DOTFILES_DIR="$HOME/.dotfiles"
 
 echo "-> Cloning dotfiles..."
