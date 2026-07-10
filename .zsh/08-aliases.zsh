fe0b40e69e16090215dfd92ba18be0cb99ac87ab
Osaigbovo Omere <dkprbw5k@anonaddy.me>
2026-07-10 23:06:27 +0100

    updates

--- .zsh/08-aliases.zsh
+++ .zsh/08-aliases.zsh
@@ -51,6 +51,7 @@ alias -g G='| grep -i'
 alias -g WC='| wc -l'
 alias -g NUL='>/dev/null 2>&1'
 alias -g E='2>&1'
+alias -g COPY='| wl-copy'
 
 alias fullcharge='sudo tlp fullcharge'
 alias paste='wl-paste'
